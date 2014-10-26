b64811f218b3c8fe88cef0e0149b5dec0155f579
dustin-H <dustin.hoffner@gmail.com>
2014-10-26 17:43:18 +0100

    commenting

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
+// Loading Dependencies
 var express = require('express');
-require('socket.io-client');
+var sio = require('socket.io');
+
+// Create Express Application
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+// Host folder /public as WebServer
+app.use(express.static(__dirname + '/public')); 
 
+// Make 'socket.io.js' file available for client
 app.get('/socket.io.js', function (req, res) {
     res.sendFile(__dirname + '/node_modules/socket.io-client/socket.io.js');
 });
 
-var io = require('socket.io').listen(app.listen(8080));
+// Create Socket.IO and Express Server
+var io = sio.listen(app.listen(8080));
 
-var counter = 0;
-
+// Listening for a new Socket.IO connection
 io.sockets.on('connection', function (socket) {
     console.log('User Connected ' + socket.id);
     
+    // Set username to socket.io session
     socket.username = socket.request._query.username;
+    
+    // Broadcast new user
     io.sockets.emit('msg', {
         username: "SERVER",
         msg: socket.username+ " logged in!"
     });
-
+    
+    // receive messages from socket (listening to 'msg' event)
     socket.on('msg', function (data) {
-        console.log(socket.username+" => "+data);
+        // Broadcast user message
         io.sockets.emit('msg', {
             username: socket.username,
             msg: data
         });
     });
-
+    
+    // Listen and Log user disconnect
     socket.on('disconnect', function () {
         console.log('Disconnected ' + socket.id);
     });
